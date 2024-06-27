0dbede9d4f539522d163c18f214b97bb7ecdd306
Laura Ferrini <laura.ferrini01@gmail.com>
2024-06-27 12:19:59 +0200

    new psm with ticks

--- data/simil_mat_comparison.R
+++ data/simil_mat_comparison.R
@@ -1,65 +1,26 @@
 ######################
-# some exploratory data analysis on cluster membership 
-
-load("S100K_round1.RData")
-out1 = out_mcmc
-load("S100K_round2.RData")
-out2 = out_mcmc
-source("clustering_functions.R")
-# vi1    =  minVI(out1$simil_mat)$cl
-# vi2    =  minVI(out2$simil_mat)$cl
-
-clus_hat1 = from_simil_to_clust(out1$simil_mat)
-clus_hat2 = from_simil_to_clust(out2$simil_mat)
-
-ordered_individuals1 = unlist(sapply(1:length(table(clus_hat1)),
-                                     function(k) c(which(clus_hat1 == k))))
-
-
-simil_ordered1 = out1$simil_mat[ordered_individuals1,ordered_individuals1]
-simil_ordered2 = out2$simil_mat[ordered_individuals1,ordered_individuals1]
-
-
-plot(out1$simil_mat, out2$simil_mat)
-library(fields)
-#par(mfrow=c(1,2))
-colori = colorRampPalette(c('white','black'))
-#image(simil_ordered1)
-#image(simil_ordered2)
-
-image.plot(simil_ordered1, col = colori(100), zlim = c(0,1), cex.sub = 1.5, xlab = "", 
-           axes = F, horizontal = F, legend.shrink = 1)
-# With Variation of Information
+# Posterior similarity Matrix with individuals ordered accordingly to estimated point 
+# clustering structure, obtained via Variation of Information criterion
 library(mcclust.ext)
-vi1 = minVI(out1$simil_mat)$cl
-vi2 = minVI(out2$simil_mat)$cl
-sum(vi1 == 1)
-sum(vi1 == 2)
-ordered_ind1_vi = unlist(sapply(1:length(table(vi1)),
-                                     function(k) c(which(vi1 == k))))
+library(fields)
+load("data/out_breastcancer1.RData")
+str(out_mcmc)
 
+vi = minVI(out_mcmc$simil_mat)$cl
 
-simil_ordered1_vi = out1$simil_mat[ordered_ind1_vi,ordered_ind1_vi]
-simil_ordered2_vi= out2$simil_mat[ordered_ind1_vi,ordered_ind1_vi]
+sum(vi == 1) # n_1
+sum(vi == 2) # n_2
+ordered_ind1_vi = unlist(sapply(1:length(table(vi)),
+                                     function(k) c(which(vi == k))))
+
+simil_ordered1_vi = out_mcmc$simil_mat[ordered_ind1_vi,ordered_ind1_vi]
 
 dev.off()
-pdf(file = "psm_round1.pdf",   # The directory you want to save the file in
-    width = 8, # The width of the plot in inches
-    height = 7.6) # The height of the plot in inches
-image.plot(simil_ordered1_vi, col = colori(100), zlim = c(0,1), cex.sub = 3, 
-           axes = F, horizontal = F, legend.shrink = 1, xlab = " Individuals (i)",
-           ylab = "Individuals (i')", cex.axis = 4 )
-
-
-
-
-########
-n = length(vi1)
+n = length(vi)
 grid_labs = c(1, round(n/4), n/2, round(3*n/4), n)
 pos_labs  = grid_labs/n; pos_labs[1] = 0
 
 library(fields)
-
 
 dev.off()
 pdf(file = "psm_round1.pdf",   # The directory you want to save the file in
